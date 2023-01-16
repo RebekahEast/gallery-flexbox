8eccc3d86e0543bdadfd19d6c95b9edb155de69c
RebekahEast <rebekah.jean.east@gmail.com>
2023-01-16 13:58:23 +0000

    commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function setNewImageAngus () 
+{ 
+    document.getElementById("img1").src = "pictures/nelore-cow.jpeg"
+}
+function setOldImageAngus () 
+{ 
+    document.getElementById("img1").src = "pictures/angus"
+}
