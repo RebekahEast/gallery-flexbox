4229d765ba7b46375ef3861cd0e94c5a36e482d0
RebekahEast <rebekah.jean.east@gmail.com>
2023-01-16 16:36:11 +0000

    hover setup

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ANGUS COW
 function setNewImageAngus () 
 { 
     document.getElementById("img1").src = "pictures/angus-info.png"
@@ -7,12 +8,22 @@ function setOldImageAngus ()
     document.getElementById("img1").src = "pictures/angus-cow.jpeg"
 }
 
-
+// ENGLISH LONGHORN
 function setNewImageEnglishLonghorn () 
 { 
-    document.getElementById("img2").src = "pictures/angus-info.png"
+    document.getElementById("img2").src = "pictures/nguni-cow.jpg"
 }
-function setOldImageEnglishLonghorn () 
+function setOldImageEnglishLonghorn ()
 { 
-    document.getElementById("img2").src = "pictures/angus-cow.jpeg"
+    document.getElementById("img2").src = "pictures/English-longhorn-cow.jpeg"
+}
+
+// GUERNSEY
+function setNewImageGuernsey() 
+{ 
+    document.getElementById("img3").src = "pictures/nguni-cow.jpg"
+}
+function setOldImageGuernsey() 
+{ 
+    document.getElementById("img3").src = "pictures/Guernsey-cow.jpeg"
 }
